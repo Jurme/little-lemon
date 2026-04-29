d3f7f458ef86fe62d2f4d4a62094cf2b75743eac
Jurme Tenzin <jurmetenzin@Jurmes-MacBook-Pro.local>
2026-04-29 13:47:05 +0600

    meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import logo from './logo.svg';
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Little Lemon</h1>
-    </div>
+    <>
+    <header>
+      <img src={logo} height={40}  width={40} alt="Little Lemon Logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservation</li>
+          <li>Order Online</li>
+          <li>Login</li>
+         </ul>
+      </nav>
+    </header>
+
+    <footer>
+      <p> All right reserved</p>
+    </footer>
+    </>
+
   );
 }
 
